7af1f754a349d02af512089ca6f989f7c6639c3c
ickee953 <ickee953@gmail.com>
2025-06-09 13:32:42 +0300

    Room database singleton.

---
+++ app/src/main/java/ru/samsmu/app/data/db/SamSmuDB.kt
@@ -0,0 +1,37 @@
+/**
+ * © Panov Vitaly 2025 - All Rights Reserved
+ *
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Panov Vitaly 9 Jun 2025
+ */
+
+package ru.samsmu.app.data.db
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+abstract class SamSmuDB : RoomDatabase() {
+
+    companion object {
+        private var INSTANCE: SamSmuDB? = null
+        private const val DB_NAME = "samsmu_db.db"
+
+        fun getDatabase( context: Context): SamSmuDB {
+            if( INSTANCE == null ){
+                synchronized( SamSmuDB::class.java ){
+                    if( INSTANCE == null ){
+                        INSTANCE = Room.databaseBuilder(
+                            context.applicationContext,
+                            SamSmuDB::class.java, DB_NAME
+                        ).build()
+                    }
+                }
+            }
+
+            return INSTANCE!!
+        }
+    }
+
+}
